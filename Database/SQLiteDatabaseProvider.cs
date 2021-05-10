e37d8df40cbb2da4a13cf6c8c064b950cb025e4b
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2021-05-10 11:21:09 +0200

    sqlite core

---
+++ Database/SQLiteDatabaseProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Text;
+
+namespace PriateCardGame.Database
+{
+    class SQLiteDatabaseProvider
+    {
+        private readonly string connectionString;
+
+        public SQLiteDatabaseProvider(string Connectionstring)
+        {
+            this.connectionString = Connectionstring;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
