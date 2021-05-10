4c43a4c6542399cd93c6d93ff7e53cbbf26c698d
JohnnySii <johnnysii39@gmail.com>
2021-05-10 12:18:18 +0200

    CardBase

--- CardBase.cs
+++ CardBase.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PriateCardGame
 {
-    class CardBase
+    public class CardBase : GameObject
     {
+        public string Name { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Health { get; set; }
+
+        public int CardID { get; set; }
+
+
+        //Validate method?
+
+        public void CardEffect()
+        {
+
+        }
+        public override void LoadContent(ContentManager contentManager)
+        {
+            this.sprite = contentManager.Load<Texture2D>("PirateCaptain");
+            this.color = Color.White;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, color);
+        }
+
     }
 }
