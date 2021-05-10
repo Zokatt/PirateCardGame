0430262ff49cd71e77fd5db76d116b9c24d7476b
JohnnySii <johnnysii39@gmail.com>
2021-05-10 12:22:44 +0200

    Extra gameobject forgotten to save

--- GameObject.cs
+++ GameObject.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PriateCardGame
 {
-    class GameObject
+    public abstract class GameObject
     {
+        public Vector2 position;
+        public Texture2D sprite;
+        public Color color;
+
+        public virtual Rectangle Collision
+        {
+            get
+            {
+                return new Rectangle(
+                       (int)position.X,
+                       (int)position.Y,
+                       (int)sprite.Width,
+                       (int)sprite.Height
+                   );
+            }
+        }
+        public abstract void LoadContent(ContentManager contentManager);
+
+        public abstract void Update(GameTime gameTime);
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, color);
+        }
     }
 }
