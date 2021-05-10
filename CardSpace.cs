c4d8573d516e7ce7c44815ef26108f5f7ccee415
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2021-05-10 14:00:02 +0200

    cardspace

---
+++ CardSpace.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriateCardGame
+{
+    public class CardSpace
+    {
+        private CardBase card;
+        public Vector2 position;
+
+        public void setCar(CardBase otherCard)
+        {
+            this.card = otherCard;
+            this.card.position = this.position;
+        }
+
+        public void DrawCard(SpriteBatch spritebatch)
+        {
+            card.Draw(spritebatch);
+        }
+
+    }
+}
