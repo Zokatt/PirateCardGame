0db0a72d1a39984cdf308d8b37cfb87e3d488247
Nikolaj Hornung Larsen <10714@student.eadania.dk>
2021-05-11 13:42:28 +0200

    pirate captain special effect

--- Cards/Captain.cs
+++ Cards/Captain.cs
@@ -11,7 +11,27 @@ namespace PriateCardGame.Cards
     {
         public override void AdditionalCardEffect(List<CardSpace> enemySpaces, List<CardSpace> playerSpaces)
         {
-            
+            Random rnd = new Random();
+            if (this.position.Y <500)
+            {
+                foreach (CardSpace item in playerSpaces)
+                {
+                    if (item.card!=null)
+                    {
+                        item.card.Health -= rnd.Next(0,5);
+                    }
+                }
+            }
+            else
+            {
+                foreach (CardSpace item in enemySpaces)
+                {
+                    if (item.card!=null)
+                    {
+                        item.card.Health -= rnd.Next(0, 5);
+                    }
+                }
+            }
         }
 
         public Captain()
@@ -24,6 +44,15 @@ namespace PriateCardGame.Cards
         {
             this.sprite = contentManager.Load<Texture2D>("PirateCaptain");
             this.color = Color.White;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, null, color, 0f,
+            Vector2.Zero, 0.5f, SpriteEffects.None, 0f);
+
+            spriteBatch.DrawString(GameWorld.font, $"{this.Damage}", new Vector2(this.position.X + 17, this.position.Y + 160), Color.Black);
+            spriteBatch.DrawString(GameWorld.font, $"{this.Health}", new Vector2(this.position.X + 100, this.position.Y + 160), Color.Goldenrod);
         }
     }
 }
