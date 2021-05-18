1334909a963c725cca7c81d9b46075b31a613853
JohnnySii <johnnysii39@gmail.com>
2021-05-18 12:48:27 +0200

    Fat pirate sprite class and sprite added

---
+++ Cards/FatPirate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriateCardGame.Cards
+{
+    class FatPirate : CardBase
+    {
+        public override void AdditionalCardEffect(List<CardSpace> enemySpaces, List<CardSpace> playerSpaces)
+        {
+            
+        }
+
+        public FatPirate()
+        {
+            this.Damage = 3;
+            this.Health = 6;
+        }
+
+        public override void LoadContent(ContentManager contentManager)
+        {
+            this.sprite = contentManager.Load<Texture2D>("FatPirate"); //Needs a farpirate sprite
+            this.color = Color.White;
+            this.DamageBox = contentManager.Load<Texture2D>("DamageBox");
+        }
+    }
+}
