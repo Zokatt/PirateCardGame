5d75da1e8e61963e41931cf52f8d3c7aa14ace1c
JohnnySii <johnnysii39@gmail.com>
2021-05-31 19:31:19 +0200

    Change of getting random card
    Depending on the strength of the card, the cards have less/more chacne to drop.

--- Cards/DavyJonesLocker.cs
+++ Cards/DavyJonesLocker.cs
@@ -14,40 +14,80 @@ namespace PriateCardGame.Cards
         {
             Random rnd = new Random();
             int rndCard = rnd.Next(1, 10);
-            
+            int rndCardToHand = rnd.Next(0,101);
 
             if (this.Health <= 0)
             {
-                switch (rndCard)
+                //switch (rndCard)
+                //{
+                //    case 1:
+                //        tmpCard = new Cannibal();
+                //        break;
+                //    case 2:
+                //        tmpCard = new Cannon();
+                //        break;
+                //    case 3:
+                //        tmpCard = new Captain();
+                //        break;
+                //    case 4:
+                //        tmpCard = new DavyJonesLocker();
+                //        break;
+                //    case 5:
+                //        tmpCard = new FatPirate();
+                //        break;
+                //    case 6:
+                //        tmpCard = new Musketeer();
+                //        break;
+                //    case 7:
+                //        tmpCard = new Swapper();
+                //        break;
+                //    case 8:
+                //        tmpCard = new Thief();
+                //        break;
+                //    case 9:
+                //        tmpCard = new Whale();
+                //        break;
+                //}
+
+
+                if (rndCardToHand < 40)
                 {
-                    case 1:
-                        tmpCard = new Cannibal();
-                        break;
-                    case 2:
-                        tmpCard = new Cannon();
-                        break;
-                    case 3:
-                        tmpCard = new Captain();
-                        break;
-                    case 4:
-                        tmpCard = new DavyJonesLocker();
-                        break;
-                    case 5:
-                        tmpCard = new FatPirate();
-                        break;
-                    case 6:
-                        tmpCard = new Musketeer();
-                        break;
-                    case 7:
-                        tmpCard = new Swapper();
-                        break;
-                    case 8:
-                        tmpCard = new Thief();
-                        break;
-                    case 9:
-                        tmpCard = new Whale();
-                        break;
+                    tmpCard = new Swapper();
                 }
+                if (rndCardToHand < 60 && rndCardToHand >= 40)
+                {
+                    tmpCard = new Cannibal();
+                }
+                if (rndCardToHand < 75 && rndCardToHand >= 60)
+                {
+                    tmpCard = new Musketeer();
+                }
+                if (rndCardToHand < 85 && rndCardToHand >= 75)
+                {
+                    tmpCard = new Thief();
+                }
+                if (rndCardToHand < 90 && rndCardToHand >= 85)
+                {
+                    tmpCard = new DavyJonesLocker();
+                }
+                if (rndCardToHand < 93 && rndCardToHand >= 90)
+                {
+                    tmpCard = new Cannon();
+                }
+                if (rndCardToHand < 96 && rndCardToHand >= 93)
+                {
+                    tmpCard = new FatPirate();
+                }
+                if (rndCardToHand < 100 && rndCardToHand >= 96)
+                {
+                    tmpCard = new Whale();
+                }
+                if (rndCardToHand == 100)
+                {
+                    tmpCard = new Captain();
+                }
+
+
                 if (this.position.Y < 500)
                 {
                     GameWorld.enemy.EnemyHand.Add(tmpCard);
